99f4ad90aac65344009cca1851501d0c8bc9a774
Jordan Knuts <j.knuts@outlook.com>
2022-02-12 12:08:16 +0000

    handleClick: write X/O simultaneously

--- tictactoe/src/Component/GamePlay.js
+++ tictactoe/src/Component/GamePlay.js
@@ -5,12 +5,20 @@ export default class GamePlay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     
+        step: 0,
+        xTurn: true,
+        currentSquares:Array(9).fill(null) ,
     };
   }
 
   handleClick(i) {
-    console.log('i', i)
+    const squares =  this.state.currentSquares;
+    squares[i] = this.state.xTurn ? "X" : "O";
+    this.setState({
+      currentSquares: squares,
+      xTurn: !this.state.xTurn,
+      step: this.state.step+1,
+    });
   }
 
   render() {
@@ -21,7 +29,7 @@ export default class GamePlay extends Component {
         <div className="game-board">
           <GameBoard
             onClick={(i) => this.handleClick(i)}
-            squares={Array(9).fill(null)}
+            squares={this.state.currentSquares}
           />
         </div>
       </div>
